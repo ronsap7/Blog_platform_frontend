e40ddd7af743d89ab418ee9ef9dcb3703166b003
fredegd <124848643+fredegd@users.noreply.github.com>
2023-06-12 12:59:55 +0200

    basic routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import InputMask from "./components/InputMask";
+import BlogList from "./components/BlogList";
+import BlogView from "./components/BlogView";
 
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
   return (
     <>
-     </>
-  )
+      <h1>CROSSOVER BLOG</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BlogList />} />
+        <Route path="/create" element={<InputMask />} />
+        <Route path="/blogs/:id" element={<BlogView />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/BlogList.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import PageTitle from "./PageTitle";
+
+export default function BlogList() {
+  return (
+    <div>BlogList</div>
+  )
+}
