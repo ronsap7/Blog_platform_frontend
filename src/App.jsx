52a700de1ad5319336923ac252456de7fe9d8f66
fredegd <124848643+fredegd@users.noreply.github.com>
2023-06-12 14:38:45 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navigation from "./components/Navigation";
 import InputMask from "./components/InputMask";
 import BlogList from "./components/BlogList";
 import BlogView from "./components/BlogView";
@@ -12,10 +12,11 @@ function App() {
   return (
     <>
       <h1>CROSSOVER BLOG</h1>
-      <Navbar />
+      <Navigation />
       <Routes>
         <Route path="/" element={<BlogList />} />
         <Route path="/create" element={<InputMask />} />
+        <Route path="/blogs" element={<BlogList />} />
         <Route path="/blogs/:id" element={<BlogView />} />
       </Routes>
     </>
